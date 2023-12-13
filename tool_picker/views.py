11c2533842802a0dcf9af6c559a8c280905b8c55
Jonathan Garro <jonathan.garro@gmail.com>
2023-12-13 09:08:02 -0500

    update view structure

--- tool_picker/views.py
+++ tool_picker/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def home(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
