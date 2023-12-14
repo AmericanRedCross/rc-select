2c6ae59ea1920bca5a32db82e8ff083ce06fb192
Jonathan Garro <jonathan.garro@gmail.com>
2023-12-14 08:00:30 -0500

    update installed apps

--- rc_select/settings.py
+++ rc_select/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'tool_picker.apps.ToolPickerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
