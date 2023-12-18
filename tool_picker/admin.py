744aa23a2b7b02f19e1c34de07bfd0f0e68eee4f
Jonathan Garro <jonathan.garro@gmail.com>
2023-12-18 14:27:16 -0500

    add tool definitions to admin

--- tool_picker/admin.py
+++ tool_picker/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Tool
+from .models import Tool, ToolAttributeDefinition
 
 admin.site.register(Tool)
+admin.site.register(ToolAttributeDefinition)
+admin.site.site_header = "RC Select Admin Portal"
