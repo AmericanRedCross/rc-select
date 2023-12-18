3b5ed40b0e147ffbd2c2245acc1f94624564b9ea
Jonathan Garro <jonathan.garro@gmail.com>
2023-12-18 14:27:40 -0500

    update urls

--- rc_select/urls.py
+++ rc_select/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from tool_picker import views
+from users import views as user_views
+from tool_picker import views as tool_picker_views
 
 urlpatterns = [
-    path('tool_picker/', include("tool_picker.urls")),
+    path('', tool_picker_views.home, name='index'),
+    path('about/', tool_picker_views.about, name='tool-picker-about'),
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('get_cost_data/', tool_picker_views.get_cost_data, name='get_cost_data'),
+    path('tool/', tool_picker_views.tool_info, name='tool-info'),
+    path('tool_selection/', tool_picker_views.tool_selection_walkthrough, name='tool_selection_begin'),
+    path('tool_picker_results/', tool_picker_views.tool_picker_results, name='tool_picker_results'),
 ]
