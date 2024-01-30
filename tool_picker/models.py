f14f48e8963e11360bdf876e668109b554d45ef4
Jonathan Garro <jonathan.garro@gmail.com>
2024-01-30 16:04:52 -0500

    update models

--- tool_picker/models.py
+++ tool_picker/models.py
@@ -136,71 +136,91 @@ class Tool(models.Model):
             MaxValueValidator(4, message="Setup speed cannot be greater than 4"),
         ])
     setup_speed_desc = models.TextField(default="No description provided.")
+    
     setup_complexity = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Setup complexity cannot be less than 0"),
             MaxValueValidator(4, message="Setup complexity cannot be greater than 4"),
         ])
     setup_complexity_desc = models.TextField(default="No description provided.")
+    
     maintenance_complexity = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Maintenance complexity cannot be less than 0"),
             MaxValueValidator(4, message="Maintenance complexity cannot be greater than 4"),
         ])
     maintenance_complexity_desc = models.TextField(default="No description provided.")
+    
     training_and_support = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Training and support cannot be less than 0"),
             MaxValueValidator(4, message="Training and support cannot be greater than 4"),
         ])
     training_and_support_desc = models.TextField(default="No description provided.")
+    
     transition = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Transition cannot be less than 0"),
             MaxValueValidator(4, message="Transition cannot be greater than 4"),
         ])
     transition_desc = models.TextField(default="No description provided.")
+    
     performance = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Performance cannot be less than 0"),
             MaxValueValidator(4, message="Performance cannot be greater than 4"),
         ])
     performance_desc = models.TextField(default="No description provided.")
+    
     connectivity = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Connectivity cannot be less than 0"),
             MaxValueValidator(4, message="Connectivity cannot be greater than 4"),
         ])
     connectivity_desc = models.TextField(default="No description provided.")
+    
     data_cleaning = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Data cleaning cannot be less than 0"),
             MaxValueValidator(4, message="Data cleaning cannot be greater than 4"),
         ])
     data_cleaning_desc = models.TextField(default="No description provided.")
+    
     data_viz = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Data visualization cannot be less than 0"),
             MaxValueValidator(4, message="Data visualization cannot be greater than 4"),
         ])
     data_viz_desc = models.TextField(default="No description provided.")
+    
     data_management_policies = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Data management policies cannot be less than 0"),
             MaxValueValidator(4, message="Data management policies cannot be greater than 4"),
         ])
     data_management_policies_desc = models.TextField(default="No description provided.")
+    
     interoperability = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Interoperability cannot be less than 0"),
             MaxValueValidator(4, message="Interoperability cannot be greater than 4"),
         ])
     interoperability_desc = models.TextField(default="No description provided.")
+    
     localization = models.IntegerField(default=0, validators=[
             MinValueValidator(0, message="Localization cannot be less than 0"),
             MaxValueValidator(4, message="Localization cannot be greater than 4"),
         ])
     localization_desc = models.TextField(default="No description provided.")
-    data_security = models.IntegerField(default=0, validators=[
-            MinValueValidator(0, message="Data security cannot be less than 0"),
-            MaxValueValidator(4, message="Data security cannot be greater than 4"),
+    
+    data_privacy = models.IntegerField(default=0, validators=[
+            MinValueValidator(0, message="Data privacy cannot be less than 0"),
+            MaxValueValidator(4, message="Data privacy cannot be greater than 4"),
         ])
-    data_security_desc = models.TextField(default="No description provided.")
+    data_privacy_desc = models.TextField(default="No description provided.")
+    
+    data_protection = models.IntegerField(default=0, validators=[
+            MinValueValidator(0, message="Data protection cannot be less than 0"),
+            MaxValueValidator(4, message="Data protection cannot be greater than 4"),
+        ])
+    data_protection_desc = models.TextField(default="No description provided.")
+    
     cost = models.IntegerField(default=0, validators=[
         MinValueValidator(0, message="Cost cannot be less than 0"),
         MaxValueValidator(4, message="Cost cannot be greater than 4"),
         ])
     cost_desc = models.TextField(default="No description provided.")
+    
     date_added = models.DateTimeField(default=timezone.now)
     added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     
