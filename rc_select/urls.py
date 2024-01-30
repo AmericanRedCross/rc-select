1998441be932d12a703c5b48b747ad65dd713f92
Jonathan Garro <jonathan.garro@gmail.com>
2024-01-30 16:04:14 -0500

    tool picker result path

--- rc_select/urls.py
+++ rc_select/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('get_support_data/', tool_picker_views.get_support_data, name='get_support_data'),
     path('tool/', tool_picker_views.tool_info, name='tool-info'),
     path('tool_selection/', tool_picker_views.tool_selection_walkthrough, name='tool_selection_begin'),
-    path('tool_picker_results/', tool_picker_views.tool_picker_results, name='tool_picker_results'),
+    path('tool_picker_results/<str:response_id>', tool_picker_views.tool_picker_results, name='tool_picker_results'),
 ]
