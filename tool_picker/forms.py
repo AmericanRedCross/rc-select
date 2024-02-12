c73d01544b72cf41e57a623e36789961eb017ba3
Jonathan Garro <jonathan.garro@gmail.com>
2024-02-12 15:15:33 -0500

    add upload file form class

--- tool_picker/forms.py
+++ tool_picker/forms.py
@@ -5,3 +5,6 @@ class ToolSelectionForm(forms.ModelForm):
     class Meta:
         model = ToolPicker
         fields = ['intended_use_type', 'available_budget', 'setup_time', 'setup_complexity', 'maintenance', 'closeout', 'support', 'performance', 'connectivity', 'data_cleaning', 'data_viz', 'interoperability', 'localization', 'data_privacy', 'data_protection']
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
