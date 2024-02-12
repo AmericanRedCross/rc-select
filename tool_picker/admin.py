e96f8fcd6b69d136487f9bbdf83342bf6d34ba2d
Jonathan Garro <jonathan.garro@gmail.com>
2024-02-12 12:12:56 -0500

    register tool features for admin

--- tool_picker/admin.py
+++ tool_picker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Tool, ToolAttributeDefinition
+from .models import Tool, ToolAttributeDefinition, ToolFeature
 
 admin.site.register(Tool)
 admin.site.register(ToolAttributeDefinition)
+admin.site.register(ToolFeature)
 admin.site.site_header = "RC Select Admin Portal"
