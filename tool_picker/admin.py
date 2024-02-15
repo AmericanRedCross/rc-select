3b8cff324e1780ed9ea999426309b99ca6c72c80
Jonathan Garro <jonathan.garro@gmail.com>
2024-02-15 11:38:25 -0500

    register ToolResource to admin page

--- tool_picker/admin.py
+++ tool_picker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Tool, ToolAttributeDefinition, ToolFeature
+from .models import Tool, ToolAttributeDefinition, ToolFeature, ToolResource
 
 admin.site.register(Tool)
 admin.site.register(ToolAttributeDefinition)
 admin.site.register(ToolFeature)
+admin.site.register(ToolResource)
 admin.site.site_header = "RC Select Admin Portal"
