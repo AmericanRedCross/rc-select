ccc10eaa8747bde4fef0952c3fc8d7c00f81e853
Jonathan Garro <jonathan.garro@gmail.com>
2024-04-25 14:09:43 -0400

    update db to render

--- rc_select/settings.py
+++ rc_select/settings.py
@@ -70,12 +70,24 @@ WSGI_APPLICATION = 'rc_select.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rcselectstagingdb',
+        'USER': 'rcselectstagingdb_user',
+        'PASSWORD': os.getenv('DB_PASSWORD', 'db_password_not_found'),
+        'HOST': os.getenv('DB_HOSTNAME', 'db_hostname_not_found'),
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
