b41a0c1bbaad2cc9318800d8ab7c5ed8b275e41d
Tom Jordan <tom.jordan@wisc.edu>
2024-09-05 12:20:21 -0500

    ran into issues with staticfiles_storage type, so removing the manifest component

--- rc_select/settings.py
+++ rc_select/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
